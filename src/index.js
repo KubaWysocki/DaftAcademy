15726eb6c98a9e3aeb53f1b70852d95def3e53fe
Kuba Wysocki <jestemjakubwysocki@gmail.com>
2019-04-09 23:08:00 +0200

    clock generator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-//import script from './script'
 import './sass/main.scss'
-//script()
+
+function* gen( [ h, m ,s ] ) {
+    const clock = document.querySelector('#clock')
+    while( true ) {
+        s++
+        if( s > 59 ) { m++; s = 0 }
+        if( m > 59 ) { h++; m = 0 }
+        if( h > 11 ) h -= 12
+        yield clock.innerHTML = [ h, m, s ].map( el => el.toString().padStart( 2, '0' ) ).join( ':' ) 
+    }
+}
+const timer = new gen( new Date().toLocaleTimeString().split( ':' ).map( el => +el ) )
+setInterval( () => timer.next(), 1000 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,47 @@
-const path = require('path')
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    entry: {
-        main: './src/index.js'
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve('./build')
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-              test: /\.s(a|c)ss$/,
-              use: [
-                isProduction
-                ? MiniCssExtractPlugin.loader
-                : { loader: 'style-loader', options: { sourceMap: true } },
-                { loader: 'css-loader', options: { sourceMap: isProduction } },
-                { loader: 'postcss-loader', options: { sourceMap: isProduction } },
-                { loader: 'sass-loader', options: { sourceMap: isProduction } }
-              ]
-            },
-            {
-              test: /\.m?js$/,
-              exclude: /(node_modules|bower_components)/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env']
-                }
+  entry: ["@babel/polyfill", "./src/index.js"],
+  output: {
+      filename: 'main.js',
+      path: path.resolve('./build')
+  },
+  mode: 'development',
+  module: {
+      rules: [
+          {
+            test: /\.s(a|c)ss$/,
+            use: [
+              isProduction
+              ? MiniCssExtractPlugin.loader
+              : { loader: 'style-loader', options: { sourceMap: true } },
+              { loader: 'css-loader', options: { sourceMap: isProduction } },
+              { loader: 'postcss-loader', options: { sourceMap: isProduction } },
+              { loader: 'sass-loader', options: { sourceMap: isProduction } }
+            ]
+          },
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
               }
             }
-          ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        hash: true,
-        title: 'Daftacademy Drink Base',
-        template: './src/index.html',
-        filename: './index.html'
-      }),
-      new MiniCssExtractPlugin()
-    ]
+          }
+        ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: 'Daftacademy Drink Base',
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ]
 }
