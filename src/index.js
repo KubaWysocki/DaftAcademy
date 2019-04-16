8e10d6bb59c777fcf034afd76ff4cd25b043acbd
Kuba Wysocki <jestemjakubwysocki@gmail.com>
2019-04-16 20:38:41 +0200

    my promiseAll and promiseRace implementation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,88 @@
 import './sass/main.scss'
-function* gen( [ h, m ,s ] ) {
-    const clock = document.querySelector('#clock')
-    while( true ) {
-        s++
-        if( s > 59 ) { m++; s = 0 }
-        if( m > 59 ) { h++; m = 0 }
-        if( h > 11 ) h -= 12
-        yield clock.innerHTML = [ h, m, s ].map( el => el.toString().padStart( 2, '0' ) ).join( ':' ) 
-    }
+
+import clock from './clock'
+
+function promiseAll(promises) {
+    return new Promise(( resolve, reject ) => {
+        const all = []
+        if( !promises.length ) resolve([])
+        promises.forEach( async ( pro, i ) => {
+            await pro.then( res => { 
+                all[i] = res 
+                if(all.length === promises.length) resolve(all)
+            }).catch( err => {
+                reject(err)
+            })
+        })
+    })
 }
-const timer = new gen( new Date().toLocaleTimeString().split( ':' ).map( el => +el ) )
-setInterval( () => timer.next(), 1000 )
+
+function promiseRace(promises) {
+    return new Promise(( resolve, reject ) => {
+        promises.forEach( async pro => {
+            if( pro.then ) await pro.then( res => resolve(res) ).catch( err => reject(err) )
+            else resolve( pro )
+        })
+    })
+}
+// Kod testowy.
+promiseAll([]).then(result => {
+    console.log('To powinien być []:', JSON.stringify(result));
+});
+
+promiseAll([futureSuccess(1), futureSuccess(2), futureSuccess(3)]).then(result => {
+    console.log('To powinien być [1, 2, 3]:', result);
+});
+
+promiseAll([futureSuccess(1), Promise.reject('X'), futureSuccess(3)])
+    .then((res) => {
+            console.log(res)
+            console.log('WAT?! Nie powinno nas tu być..');
+    })
+    .catch(error => {
+        if (error !== 'X') {
+            console.log('Coś poszło nie tak..:', error);
+        }
+            console.log('To powinien być X:', error);
+    });
+
+promiseRace([1, 2, 3]).then(result => {
+    console.log('This should be 1:', result);
+});
+
+const now = performance.now();
+promiseRace([delayedSuccess(1, 300), delayedSuccess(2, 200), delayedSuccess(3, 100)])
+    .then(result => {
+        const after = performance.now();
+        const diff = after - now;
+        if (diff < 100) {
+            throw 'Za szybko!'
+        }
+        if (diff >= 200) {
+            throw 'Za wolno!'
+        }
+        console.log('To powinno być 3:', result);
+    });
+
+promiseRace([futureSuccess(1), Promise.reject('X'), futureSuccess(3)])
+    .then(() => {
+        console.log('WAT?! Nie powinno nas tu być..');
+    })
+    .catch(error => {
+        if (error !== 'X') {
+        console.log('Coś poszło nie tak..:', error);
+        }
+        console.log('To powinien być X:', error);
+    });
+
+
+function futureSuccess(val) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(val), Math.random() * 500);
+    });
+};
+function delayedSuccess(val, time) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(val), time);
+    });
+};
